fbe3358f5612c070119f789ad3eded53dd61833d
zhxib <42195197+zhxib@users.noreply.github.com>
2024-04-25 17:16:43 +0800

    Add files via upload

---
+++ precache-manifest.7300f4509bf14a839e3b018eb9a2bbc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9c55f751285eb8a100ecdda5ca52ba0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe5e53e7363143cee576",
+    "url": "/static/css/2.6db87d93.chunk.css"
+  },
+  {
+    "revision": "e3ac77d39c010fc2c5ba",
+    "url": "/static/css/main.b0b67545.chunk.css"
+  },
+  {
+    "revision": "fe5e53e7363143cee576",
+    "url": "/static/js/2.69638f65.chunk.js"
+  },
+  {
+    "revision": "e3ac77d39c010fc2c5ba",
+    "url": "/static/js/main.88d6aad6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
